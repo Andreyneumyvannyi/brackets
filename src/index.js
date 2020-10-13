bae85d98bcd79ea8810396d57d3938d7ddb6260c
Andrey Neumyvannyi <andreyneumuvanny@gmail.com>
2020-10-13 11:05:53 +0400

    feat:finished task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arrConfig = [];
+    for (i = 0; i < bracketsConfig.length; i++) {
+        arrConfig = arrConfig.concat(bracketsConfig[i]);
+    }
+    let closeBr = [];
+
+    for (j = 0; j < str.length; j++) {
+        let last = closeBr[closeBr.length - 1];
+
+        if (
+            arrConfig.indexOf(str[j]) % 2 == 0 &&
+            str[j] == arrConfig[arrConfig.indexOf(str[j]) + 1]
+        ) {
+            if (last !== str[j]) {
+                closeBr.push(str[j]);
+            } else if (last == str[j]) {
+                closeBr.pop();
+            }
+        } else if (arrConfig.indexOf(str[j]) % 2 == 0) {
+            closeBr.push(str[j]);
+        }
+
+        if (arrConfig.indexOf(str[j]) % 2 !== 0) {
+            if (arrConfig.indexOf(last) == arrConfig.indexOf(str[j]) - 1) {
+                closeBr.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+
+    if (closeBr.length == 0) {
+        return true;
+    } else return false;
+};
